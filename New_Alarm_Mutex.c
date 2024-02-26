bf904ccb1cfdada74317148fdc4fd82a81171668
Daniel Di Giovanni <dannyjdigio@gmail.com>
2024-02-25 21:46:40 -0500

    Fininshed command parser

--- New_Alarm_Mutex.c
+++ New_Alarm_Mutex.c
@@ -65,28 +65,68 @@ typedef struct command_t {
 } command_t;
 
 /**
+ * This is the data type that holds information about parsing a
+ * command. It contains the type of the command, the regular
+ * expression for the command, the number of matches within the
+ * command (that must be parsed out).
+ */
+typedef struct regex_parser {
+    command_type type;
+    const char *regex_string;
+    int expected_matches;
+} regex_parser;
+
+/**
+ * These are the regexes for the commands that we must look for.
+ */
+regex_parser regexes[] = {
+    {
+        Start_Alarm,
+        "Start_Alarm\\(([0-9]+)\\):[[:space:]]([0-9]+)[[:space:]](.*)",
+        4
+    },
+    {
+        Change_Alarm,
+        "Change_Alarm\\(([0-9]+)\\):[[:space:]]([0-9]+)[[:space:]](.*)",
+        4
+    },
+    {
+        Cancel_Alarm,
+        "Cancel_Alarm\\(([0-9]+)\\)",
+        2
+    },
+    {
+        Suspend_Alarm,
+        "Suspend_Alarm\\(([0-9]+)\\)",
+        2
+    },
+    {
+        Reactivate_Alarm,
+        "Reactivate_Alarm\\(([0-9]+)\\)",
+        2
+    },
+    {
+        View_Alarms,
+        "View_Alarms",
+        1
+    }
+};
+
+/**
  * This method takes a string and checks if it matches any of the
  * command formats. If there is no match, NULL is returned. If there
  * is a match, it parses the string into a command and returns it.
  */
 command_t* parse_command(char input[]) {
-    regex_t regex; // Holds the regex that we are testing
+    regex_t regex;             // Holds the regex that we are testing
 
-    int reti;      // Holds the status of regex tests
+    int re_status;             // Holds the status of regex tests
 
-    // This is the data type that holds information about parsing a
-    // command. It contains the type of the command, the regular
-    // expression for the command, the number of matches within the
-    // command (that must be parsed out).
-    typedef struct regex_parser {
-        command_type type;
-        const char *regex_string;
-        int expected_matches;
-    } regex_parser;
-
-    regmatch_t matches[3];     // Holds the number of matches.
+    regmatch_t matches[4];     // Holds the number of matches.
                                // (No command has more than 3
-                               // matches).
+                               // matches, which we add 1 to because
+                               // the string itself counts as a
+                               // match).
 
     command_t *command;        // Holds the pointer to the command
                                // that will be returned.
@@ -95,42 +135,13 @@ command_t* parse_command(char input[]) {
 
     int number_of_regexes = 6; // Six regexes for six commands
 
+    char alarm_id_buffer[64];  // Buffer used to hold alarm_id as a
+                               // string when it is being converted
+                               // to an int.
 
-    /*
-     * These are the regexes for the commands that we must look for.
-     */
-    regex_parser regexes[] = {
-        {
-            Start_Alarm,
-            "Start_Alarm\\(([0-9]+)\\):[[:space:]]([0-9])+[[:space:]](.*)",
-            3
-        },
-        {
-            Change_Alarm,
-            "Change_Alarm\\(([0-9]+)\\):[[:space:]]([0-9])+[[:space:]](.*)",
-            3
-        },
-        {
-            Cancel_Alarm,
-            "Cancel_Alarm\\(([0-9]+)\\)",
-            1
-        },
-        {
-            Suspend_Alarm,
-            "Suspend_Alarm\\(([0-9]+)\\)",
-            1
-        },
-        {
-            Reactivate_Alarm,
-            "Reactivate_Alarm\\(([0-9]+)\\)",
-            1
-        },
-        {
-            View_Alarms,
-            "View_Alarms",
-            0
-        }
-    };
+    char time_buffer[64];      // Buffer used to  hold time as a
+                               // string when it is being converted
+                               // to an int.
 
     /*
      * Loop through the regexes and test each one.
@@ -139,14 +150,14 @@ command_t* parse_command(char input[]) {
         /*
          * Compile the regex
          */
-        reti = regcomp(
+        re_status = regcomp(
             &regex,
             regexes[i].regex_string,
             REG_EXTENDED
         );
 
-        // Make sure compilation succeeded
-        if (reti != 0) {
+        // Make sure regex compilation succeeded
+        if (re_status != 0) {
             fprintf(
                 stderr,
                 "Regex %d \"%s\" did not compile\n",
@@ -159,7 +170,7 @@ command_t* parse_command(char input[]) {
         /*
          * Test input string against regex
          */
-        reti = regexec(
+        re_status = regexec(
             &regex,
             input,
             regexes[i].expected_matches,
@@ -167,14 +178,14 @@ command_t* parse_command(char input[]) {
             0
         );
 
-        if (reti == REG_NOMATCH) {
+        if (re_status == REG_NOMATCH) {
             /*
              * If no match, then free the regex and continue on with
              * the loop.
              */
             regfree(&regex);
             continue;
-        } else if (reti != 0) {
+        } else if (re_status != 0) {
             /*
              * If regex test returned nonzero value, then it was an
              * error. In this case, free the regex and stop the
@@ -195,28 +206,6 @@ command_t* parse_command(char input[]) {
              * matched a command and we can now parse it.
              */
 
-            /* // Get the alarm_id from the input (if it exists) */
-            /* alarm_id = (regexes[i].expected_matches > 0) */
-            /*     ? atoi(&input[matches[0].rm_so]) */
-            /*     : 0; */
-            /* // Get the time from the input (if it exists) */
-            /* time = (regexes[i].expected_matches > 1) */
-            /*     ? atoi(&input[matches[1].rm_so]) */
-            /*     : 0; */
-
-            /* // Copy the message from the input (if it exists) */
-            /* if (regexes[i].expected_matches > 2) { */
-            /*     strcpy(message, &input[matches[2].rm_so]); */
-            /* } */
-
-            for (int j=0; j < regexes[i].expected_matches; j++) {
-                debug_printf(
-                    "Match: %.*s\n",
-                    matches[j].rm_eo-matches[j].rm_so,
-                    &input[matches[j].rm_so]
-                );
-            }
-
             // Free the regex
             regfree(&regex);
 
@@ -233,20 +222,34 @@ command_t* parse_command(char input[]) {
              */
             command->type = regexes[i].type;
             // Get the alarm_id from the input (if it exists)
-            if (regexes[i].expected_matches > 0) {
-                command->alarm_id = atoi(&input[matches[0].rm_so]);
+            if (regexes[i].expected_matches > 1) {
+                strncpy(
+                    alarm_id_buffer,
+                    input+matches[1].rm_so,
+                    matches[1].rm_eo - matches[1].rm_so
+                );
+                command->alarm_id = atoi(alarm_id_buffer);
             } else {
                 command->alarm_id = 0;
             }
             // Get the time from the input (if it exists)
-            if (regexes[i].expected_matches > 1) {
-                command->time = atoi(&input[matches[1].rm_so]);
+            if (regexes[i].expected_matches > 2) {
+                strncpy(
+                    time_buffer,
+                    input+matches[2].rm_so,
+                    matches[2].rm_eo - matches[2].rm_so
+                );
+                command->time = atoi(time_buffer);
             } else {
                 command->time = 0;
             }
             // Copy the message from the input (if it exists)
-            if (regexes[i].expected_matches > 2) {
-                strcpy(command->message, &input[matches[2].rm_so]);
+            if (regexes[i].expected_matches > 3) {
+                strncpy(
+                    command->message,
+                    &input[matches[3].rm_so],
+                    matches[3].rm_eo - matches[3].rm_so
+                );
             }
 
             return command;
@@ -275,8 +278,8 @@ int main(int argc, char *argv[]) {
             printf("Bad command\n");
             continue;
         } else {
-            debug_printf(
-                "{type: %s, id: %d, time: %d, message: %s}\n",
+            printf(
+                "{type: %d, id: %d, time: %d, message: %s}\n",
                 command->type,
                 command->alarm_id,
                 command->time,
